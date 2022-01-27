1df9a1f73363ba0c136884f5a9b9bc67e30a77e0
Pride <55308392+quintofsteel@users.noreply.github.com>
2022-01-27 06:36:28 +0000

    Update customerInformation.py

--- customerInformation.py
+++ customerInformation.py
@@ -1,4 +1,8 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
 
 class Ui_customerInformation(object):
     def setupUi(self, customerInformation):
